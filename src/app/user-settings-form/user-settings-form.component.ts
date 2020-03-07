173963acd4e5d19e8453a8f3be7a4cace68bd8a6
Carlos Molina <molina.carlos.dev@gmail.com>
2020-03-07 08:00:18 -0600

    Styled the VALIDATION for the form using ngSubmit along with form.submitted flag

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm, NgModel } from "@angular/forms";
 import { IUserSettings } from "../data/interface-user-settings";
 @Component({
   selector: "app-user-settings-form",
@@ -17,4 +18,11 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onBlur(field: NgModel) {
+    console.log("in blur:", field.valid);
+  }
+  onSubmit(userForm: NgForm) {
+    console.log("in OnSubmit: ", userForm.errors);
+  }
 }
