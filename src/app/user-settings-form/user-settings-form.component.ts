a54f7172bb77c10eb31dc60e03a8e26f38930a42
Carlos Molina <molina.carlos.dev@gmail.com>
2020-03-07 00:32:12 -0600

    Added an Inteface for our UserSettings and initialized it on our component

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
-
+import { IUserSettings } from "../data/interface-user-settings";
 @Component({
   selector: "app-user-settings-form",
   templateUrl: "./user-settings-form.component.html",
   styleUrls: ["./user-settings-form.component.css"]
 })
 export class UserSettingsFormComponent implements OnInit {
+  userSettings: IUserSettings = {
+    name: "Carlos",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "here are some notes"
+  };
   constructor() {}
 
   ngOnInit() {}
